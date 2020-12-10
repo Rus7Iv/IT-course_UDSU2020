355d52cbdf1bf5a1b4e3fb12f3e14077a1a3595e
Rus7Iv <73655792+Rus7Iv@users.noreply.github.com>
2020-12-10 18:41:43 +0400

    Add files via upload

---
+++ 1 задание/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите количество дней, которое вы прожили: ");
+            var days = Console.ReadLine();
+            int dayss = Convert.ToInt32(days);
+            int year = dayss / 365;
+            int month = (dayss % 365) / 30;
+            dayss = (dayss % 365) % 30;
+            Console.WriteLine($"Ваш возраст составляет {year} лет {month} месяцев и {dayss} дней");
+            if (year >= 18)
+            {
+                Console.WriteLine($"Поздравляю! Вы совершеннолетний!");
+
+
+            }
+            else
+                Console.WriteLine($"Вы несовершеннолетний");
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
