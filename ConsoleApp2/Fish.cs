9cc87e59781b736eff6496c396299638942792a4
Rus7Iv <73655792+Rus7Iv@users.noreply.github.com>
2020-12-10 18:51:10 +0400

    Add files via upload

---
+++ ConsoleApp2/Fish.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Fish : Animal
+    {
+        /// <summary>
+        /// Class of Fish
+        /// </summary>
+        public string Color;
+        public Fish(string name) : base(name)
+        {
+            _name = name;
+            Name = "Карп";
+            Weight = 16;
+            Age = 6;
+            Color = "Зеркальный";
+        }
+        public virtual void Fisher()
+        {
+
+            Console.WriteLine($"{Color} {Name} весит {Weight} килограммов и ему {Age} лет");
+            Console.WriteLine($"{Color} {Name} довольно редкий, но вкусный");
+        }
+        /// <summary>
+        /// Transform Fish characteristics to string
+        /// </summary>
+        /// <returns>String like "Name:{Color} {Name}, age: {Age}, weight: {Weight}";"</returns>
+        public override string ToString()
+        {
+            return $"Имя: {Name}, возраст: {Age}, вес: {Weight}.";
+        }
+
+
+    }
+}
